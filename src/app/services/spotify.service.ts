4d4df62df026552e548454a156320610377786a4
308 <308@academicos.cesde.edu.co>
2022-10-13 09:29:47 -0500

    Se Organiza token Ok

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -9,7 +9,7 @@ import { HttpHeaders } from '@angular/common/http';
 })
 export class SpotifyService {
   uri:string = 'https://api.spotify.com/v1/artists/34EP7KEpOjXcM2TCat1ISk/top-tracks?market=us'
-  token:string= 'Bearer BQDeFfcGmx9StXOnU1V8rT8RmOGdQss9OQ0uyVLErCOqVmFz-xTpMHJsFJzvXzbKqDZHH5k39VwYbZx6BvYuQpNbytw3O63o0ZSK6KXycrbiCrqrEqtrso6h-UxQnajb03SOJdYtFEOylgQXz6GpQlPxD-W6fAt-v8gHVJzyOmL18aFICNpaEtCD2V0WhswAhSs'
+  token:string= ' Bearer BQCqD9pBf4X_alSgn7BZNCb6g4eyvm5qS1xZl_kCqsV614qEnkhKUBjHPd2syUYsnJH15TvVAo7zdIY7oK742cbgS3u0TQF6qCIf2oyAhSBEjipBTWR1eybvr3Q9cAqQIbGHzBjaFAk7QUCmGIt8LOlwpf0sgkIXjAkk1B0lvb1Bb_CjKe3qw_8ie7HP7f_Q_OI'
 
 
   constructor(public peticion :HttpClient) {
